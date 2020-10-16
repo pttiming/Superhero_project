8c70b1387baa4a9515b4a23d59ed15bbde687bea
pttiming <sean@pttiming.com>
2020-10-15 23:17:38 -0500

    added logic to display index after creating a superhero.  Then added logic to display the details of the superhero.

--- SuperheroProject/Controllers/SuperherosController.cs
+++ SuperheroProject/Controllers/SuperherosController.cs
@@ -1,40 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SuperheroProject.Data;
 using SuperheroProject.Models;
 
 namespace SuperheroProject.Controllers
 {
-    public class SuperheroController : Controller
+    public class SuperherosController : Controller
     {
-        // GET: SuperheroController
+        private ApplicationDbContext _db;
+
+        public SuperherosController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // GET: SuperherosController
         public ActionResult Index()
         {
-            return View();
+            
+            return View(_db.Superheroes.ToList());
         }
 
-        // GET: SuperheroController/Details/5
+        // GET: SuperherosController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var superheroToShow = _db.Superheroes.Find(id);
+            return View(superheroToShow);
         }
 
-        // GET: SuperheroController/Create
+        // GET: SuperherosController/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: SuperheroController/Create
+        // POST: SuperherosController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Superhero superhero)
         {
             try
             {
+                _db.Superheroes.Add(superhero);
+                _db.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -43,19 +55,21 @@ namespace SuperheroProject.Controllers
             }
         }
 
-        // GET: SuperheroController/Edit/5
+        // GET: SuperherosController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var superheroToEdit = _db.Superheroes.Find(id);
+            return View(superheroToEdit);
         }
 
-        // POST: SuperheroController/Edit/5
+        // POST: SuperherosController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, Superhero superhero)
         {
             try
             {
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -64,13 +78,13 @@ namespace SuperheroProject.Controllers
             }
         }
 
-        // GET: SuperheroController/Delete/5
+        // GET: SuperherosController/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: SuperheroController/Delete/5
+        // POST: SuperherosController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, Superhero superhero)
